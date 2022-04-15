b5e0967b413ebcb9ecfe09efa9eb3fbaf96f76af
arnaudaploon <arnaudadjovi274@gmail.com>
2022-04-15 19:59:28 +0100

    Ajout d'une icon à l'application

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -29,6 +30,8 @@ public class App extends Application {
         try {
             root = FXMLLoader.load(getClass().getResource("fxml/LoginScene.fxml"));
             Scene scene = new Scene(root);
+
+            primaryStage.getIcons().add(new Image("gestio.png"));
 
             primaryStage.setTitle("Connexion à Gestio Eneam");
             primaryStage.setScene(scene);
